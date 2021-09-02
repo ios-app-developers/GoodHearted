b4023a60b4fe0fc556a399552b48b89ffcd5db02
Babu Rajendran <babu_rajendran@outlook.com>
2021-09-02 16:21:11 -0700

    Initial Commit

---
+++ GoodHearted/GoodHeartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodHeartedApp.swift
+//  GoodHearted
+//
+//  Created by Babu Rajendran on 9/2/21.
+//
+
+import SwiftUI
+
+@main
+struct GoodHeartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
